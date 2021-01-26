78c9117b7ee49761a3e7f89fe1b29168b7902d7b
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-01-26 11:25:22 +0000

    first draft of test pipeline with configurations for s3 bucket and dynamoDB

---
+++ pipelines/cp-1501-1106/main/build-environments/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "cloud-platform-terraform-state"
+    region = "eu-west-1"
+    key    = "concourse-pipelines/live-1/main/build-environments/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
